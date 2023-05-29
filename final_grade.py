559ee4330219fccd6314a43bb94a0ef1db8c8e9f
PongthepGeo <pongtep_tong@hotmail.com>
2023-05-29 11:56:53 +0700

    May 29

---
+++ final_grade.py
@@ -0,0 +1,83 @@
+# Import necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import numpy as np
+
+from scipy import stats
+from datetime import datetime
+
+# Read the CSV file
+@st.cache_data
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+df = load_data('raw_data/plot_distribution.csv')
+colors = {'2021': 'orange', '2022': 'green', '2023': 'black'}
+
+st.subheader(f'Score Distribution Each Year')
+fig, ax = plt.subplots()
+for column in df.columns:
+    data = df[column].dropna()  
+    kde = sns.kdeplot(data, ax=ax, color=colors[column], label=column)
+    x, y = kde.get_lines()[-1].get_data()  
+    peak_estimate = x[np.argmax(y)]  
+    ax.axvline(peak_estimate, color=colors[column], linestyle='--')
+plt.xlabel('Score')
+plt.legend()
+st.pyplot(fig)
+
+table_data = load_data('raw_data/table.csv')
+st.table(table_data)
+
+# Create a selectbox for user to select a name
+name = st.selectbox("เลือกชื่อ:", ('พงศ์เทพ',
+                                'ศาสตราจารย์ ดร.จักรพันธ์ สุทธิรัตน์',
+                                'ศาสตราจารย์ ดร.มนตรี ชูวงษ์',
+                                'ศาสตราจารย์ ดร.ศรีเลิศ โชติพันธรัตน์',
+                                'ศาสตราจารย์ ดร.พิษณุพงศ์ กาญจนพยนต์',
+                                'ศาสตราจารย์ ดร.สันติ ภัยหลบลี้',
+                                'รองศาสตราจารย์ ดร.ฐานบ ธิติมากร',
+                                'รองศาสตราจารย์ ดร.ฐาสิณีย์ เจริญฐิติรัตน์',
+                                'รองศาสตราจารย์ ดร.ปิยพงษ์ เชนร้าย',
+                                'รองศาสตราจารย์ ดร.สกลวรรณ ชาวไชย',
+                                'ผู้ช่วยศาสตราจารย์ ดร.อัคนีวุธ จิรภิญญากุล',
+                                'ผู้ช่วยศาสตราจารย์ ดร.วิชัย จูฑะโกสิทธิ์กานนท์',
+                                'ผู้ช่วยศาสตราจารย์ ดร.ฐิติพรรณ อัศวินเจริญกิจ',
+                                'ผู้ช่วยศาสตราจารย์ ดร.กันตภณ สุระประสิทธิ์',
+                                'ผู้ช่วยศาสตราจารย์ ดร.สุคนธ์เมธ จิตรมหันตกุล',
+                                'ผู้ช่วยศาสตราจารย์ ดร.อลงกต ฝั้นกา',
+                                'อาจารย์ ดร.สุเมธ พันธุวงค์ราช'
+                                ))
+
+# Create a radio button for user input
+status = st.radio("ความเห็น:", ('ไม่ออกความเห็น', 'อนุมัติ', 'ไม่อนุมัติ'))
+
+# Create a list for 'Approved' and 'Unapproved' choices for each name
+if 'status_names' not in st.session_state:
+    st.session_state['status_names'] = {'ไม่ออกความเห็น': [],
+                                        'อนุมัติ': [],
+                                        'ไม่อนุมัติ': []}
+
+# Create a button for submitting the form
+if st.button('Submit'):
+    # Update the list based on the user's choice
+    st.session_state['status_names'][status].append(name)
+    log_message = f"{datetime.now()}: User selected {name} and clicked {status}"
+
+    # Print the log message to the terminal
+    print(log_message)
+
+    # Write the log message to a file
+    with open('log.txt', 'a') as f:
+        f.write(log_message + '\n')
+
+    # Display the names
+    st.subheader('รายชื่อไม่ออกความเห็น')
+    st.write(', '.join(st.session_state['status_names']['ไม่ออกความเห็น']))
+    st.subheader('รายชื่ออนุมัติ')
+    st.write(', '.join(st.session_state['status_names']['อนุมัติ']))
+    st.subheader('รายชื่อไม่อนุมัติ')
+    st.write(', '.join(st.session_state['status_names']['ไม่อนุมัติ']))
